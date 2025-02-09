e856de9634d8b723fd918b3338fc57e1c26a6d83
kirillreuk <kirillreuk@skbkontur.ru>
2025-02-10 01:13:14 +0400

    split the app into tabs

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,10 @@ import DivisionView from './DivisionView';
 import './App.css';
 import DivisionCountInput from './inputTab/DivisionCountInput';
 
+type activeTab = 'teams' | 'divisions';
+
 const App: React.FC = () => {
+  const [activeTab, setActiveTab] = useState<activeTab>('teams');
   const [divisions, setDivisions] = useState<Team[][]>([]);
   const [conferences, setConferences] = useState<Team[][][]>([]);
   const [teams, setTeams] = useState<Team[]>(defaultTeams);
@@ -14,26 +17,31 @@ const App: React.FC = () => {
 
   return (
     <>
-      <div className="flex p-4">
-        <div className="flex-1 mr-8">
-          <TeamView teams={teams} setTeams={setTeams} />
-          <DivisionCountInput
-            teams={teams}
-            divisionsCount={divisionsCount}
-            setDivisions={setDivisions}
-            setConferences={setConferences}
-            setDivisionsCount={setDivisionsCount}
-          />
+      <button onClick={() => setActiveTab(activeTab === 'teams' ? 'divisions' : 'teams')}>Swap Tabs</button>
+      {activeTab === 'teams' ? (
+        <div className="flex p-4">
+          <div className="flex-1 mr-8">
+            <DivisionCountInput
+              teams={teams}
+              divisionsCount={divisionsCount}
+              setDivisions={setDivisions}
+              setConferences={setConferences}
+              setDivisionsCount={setDivisionsCount}
+            />
+            <TeamView teams={teams} setTeams={setTeams} />
+          </div>
         </div>
+      ) : (
+        <div className="flex p-4">
+          <div className="flex-1">
+            <DivisionView conferences={conferences} />
+          </div>
 
-        <div className="flex-1">
-          <DivisionView conferences={conferences} />
+          <div className="h-[600px] w-full">
+            <MapView divisions={divisions} />
+          </div>
         </div>
-      </div>
-
-      <div className="h-[600px] w-full">
-        <MapView divisions={divisions} />
-      </div>
+      )}
     </>
   );
 };
